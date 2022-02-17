23012fe72303fede467cc4815a1e0b417b08987b
faustovrz <faustovrz@gmail.com>
2022-02-17 10:52:01 -0500

    Fixed config file default path in config.R

--- inst/scripts/run_GLM.R
+++ inst/scripts/run_GLM.R
@@ -16,7 +16,7 @@ option_list <-c(optparse::make_option(
                  "--genotype", type = "character", default = "genotype.hmp",
                  help= "Genotype, hapmap format",
                  metavar = "character"),
-                init_option_list()
+                init_option_list(system.file("extdata", "config.yaml", pkg = "grassGEA"))
                 )
 
 usage <-  "%prog [options]"
@@ -28,6 +28,7 @@ opt_parser <- OptionParser(
 
 args <- parse_args2(opt_parser)
 opts <- args$options
+
 ################################################################################
 
 time_suffix <- format(Sys.time(), "%d-%b-%Y_%H_%M")
@@ -37,72 +38,72 @@ log_file <- paste0(opts$glm_log_prefix, time_suffix, ".log")
 print(log_file )
 q()
 
-# #Logging file
-# rTASSEL::startLogger(fullPath = opts$output_folder,
-#                      fileName = log_file )
-#
-#
-# #Load in hapmap file
-# tasGenoHMP <- rTASSEL::readGenotypeTableFromPath(
-#   path = opts$genotype
-# )
-#
-# # Read from phenotype path
-# pheno_file <- opts$phenotype
-# pheno_dir  <- opts$phenotype_folder
-# phenoPath  <- file.path(data_dir,pheno_file)
-#
-# # Load into pheno file
-# tasPheno <- rTASSEL::readPhenotypeFromPath(
-#   path = phenoPath
-# )
-#
-#
-# # Load into rTASSEL `TasselGenotypePhenotype` object
-# tasGenoPheno <- rTASSEL::readGenotypePhenotype(
-#   genoPathOrObj = tasGenoHMP,
-#   phenoPathDFOrObj = tasPheno
-# )
-#
-# #Get genotype data
-# tasSumExp <- rTASSEL::getSumExpFromGenotypeTable(
-#   tasObj = tasGenoPheno
-# )
-#
-#
-#  SummarizedExperiment::colData(tasSumExp)
-#
-#
-# #Extract phenotype data
-# tasExportPhenoDF <- rTASSEL::getPhenotypeDF(
-#   tasObj = tasGenoPheno
-# )
-#
-# tasExportPhenoDF
-#
-#
-# #Filtering genotype data
-# tasGenoPhenoFilt <- rTASSEL::filterGenotypeTableSites(
-#   tasObj = tasGenoPheno,
-#   siteMinCount = 150,
-#   siteMinAlleleFreq = 0.05,
-#   siteMaxAlleleFreq = 1.0,
-#   siteRangeFilterType = "none"
-# )
-# tasGenoPhenoFilt
-# tasGenoPheno
-#
-#
-# #Calculate GLM or MLM (add tasKin to kinship)
-# tasGLM <- rTASSEL::assocModelFitter(
-#   tasObj = tasGenoPhenoFilt,             # <- our prior TASSEL object
-#   formula = VL ~ .,                  # <- only phenotype
-#   fitMarkers = TRUE,                 # <- set this to TRUE for GLM
-#   kinship = NULL,
-#   fastAssociation = FALSE
-# )
-#
-#
-#
+#Logging file
+rTASSEL::startLogger(fullPath = opts$output_folder,
+                     fileName = log_file )
+
+
+#Load in hapmap file
+tasGenoHMP <- rTASSEL::readGenotypeTableFromPath(
+  path = opts$genotype
+)
+
+# Read from phenotype path
+pheno_file <- opts$phenotype
+pheno_dir  <- opts$phenotype_folder
+phenoPath  <- file.path(data_dir,pheno_file)
+
+# Load into pheno file
+tasPheno <- rTASSEL::readPhenotypeFromPath(
+  path = phenoPath
+)
+
+
+# Load into rTASSEL `TasselGenotypePhenotype` object
+tasGenoPheno <- rTASSEL::readGenotypePhenotype(
+  genoPathOrObj = tasGenoHMP,
+  phenoPathDFOrObj = tasPheno
+)
+
+#Get genotype data
+tasSumExp <- rTASSEL::getSumExpFromGenotypeTable(
+  tasObj = tasGenoPheno
+)
+
+
+ SummarizedExperiment::colData(tasSumExp)
+
+
+#Extract phenotype data
+tasExportPhenoDF <- rTASSEL::getPhenotypeDF(
+  tasObj = tasGenoPheno
+)
+
+tasExportPhenoDF
+
+
+#Filtering genotype data
+tasGenoPhenoFilt <- rTASSEL::filterGenotypeTableSites(
+  tasObj = tasGenoPheno,
+  siteMinCount = 150,
+  siteMinAlleleFreq = 0.05,
+  siteMaxAlleleFreq = 1.0,
+  siteRangeFilterType = "none"
+)
+tasGenoPhenoFilt
+tasGenoPheno
+
+
+#Calculate GLM or MLM (add tasKin to kinship)
+tasGLM <- rTASSEL::assocModelFitter(
+  tasObj = tasGenoPhenoFilt,             # <- our prior TASSEL object
+  formula = VL ~ .,                  # <- only phenotype
+  fitMarkers = TRUE,                 # <- set this to TRUE for GLM
+  kinship = NULL,
+  fastAssociation = FALSE
+)
+
+
+
 
 
