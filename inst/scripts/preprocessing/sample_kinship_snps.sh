a6cec506e5a9e7c63b8baaeced3ad388c5b802a1
faustovrz <faustovrz@gmail.com>
2022-02-25 21:25:38 -0500

    Added random sampling script sample_kinship_snps.sh

---
+++ inst/scripts/preprocessing/sample_kinship_snps.sh
@@ -0,0 +1,29 @@
+#/usr/bin/env bash
+
+# I need to set the random number seed in the  powershuf.py script
+# for reproducible results.
+# Branch from gitlabs?
+# https://gitlab.com/aapjeisbaas/shuf
+# I installed  powershuf.py  to
+# /usr/local/usrapps/maize/sorghum/conda/envs/r_env/bin
+
+powershuf.py --file tmp/sorghum/markers.txt -n 10000 \
+| sort -k3,3 -k 4,4 > tmp/kinship_sample.txt;
+
+# histogram
+cut -f 3 kinship_sample.txt \
+  | sort -n -k1,1 \
+  | uniq -c \
+  | perl -pe "s/ +//; s/ +/\t/" \
+  | awk ' { t = $1; $1 = $2; $2 = t; print; } ' \
+  | perl -lane 'print $F[0], "\t", "=" x ($F[1] / 50)'
+
+# there is something odd with the output.
+# I have repeated two lines forr chromosome 1
+
+
+head -n 1 tmp/sorghum/markers.txt > tmp/hapmap_header
+
+cp kinship_sample_10K.hapmap.txt /rsstu/users/r/rrellan/sara/SorghumGEA/results/TASSEL_kinship/
+
+
