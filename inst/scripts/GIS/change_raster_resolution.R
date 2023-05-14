634092381b46bfc6843c068133142d9279155a93
faustovrz <faustovrz@gmail.com>
2023-05-14 18:50:30 -0400

    Completed preprocessing of predictor variables from He2020. Except for NPP, I ran out of memory I will do it on the server.

--- inst/scripts/GIS/change_raster_resolution.R
+++ inst/scripts/GIS/change_raster_resolution.R
@@ -1,17 +1,23 @@
+# I am thinking on splitting this script by data source.
+
 # I want to upgrade the resolution of stp10
-# from 55 km (30 minutes) to 10 km (6 minutes?)
+# from 55 km (30 arc minutes) to 10 km (~5 arc minutes?)
+
 
 # I'll start by having all the predictors at 10 km resolution
 library(dplyr)
 library(raster)
 library(stars)
 
+gis_dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS"
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #### 1. PH Soil pH                                                        ####
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
+
 file <-"phh2o_0-5cm_mean_5000.tif"
 r <- raster(file.path(dir,file))
 summary(r)
@@ -44,15 +50,16 @@ summary(r)
 # Now we have decimal degree coordinates corresponding to 10 km at the equator
 # I'll scale every other lower resolution raster to this one
 
-# dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/10km/raster"
+# dir <- file.path(gis_dir,"predictors/10km/raster"
 # out_file <-"PH.tif"
 # raster::writeRaster(
 #   r,
 #   filename = file.path(dir,out_file),
-#   forrmat ="GTiff" #, overwrite=TRUE
+#   format ="GTiff" #, overwrite=TRUE
 # )
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path( gis_dir,"predictors/rfin/10km/raster")
+
 file <-"PH.tif"
 km10 <- raster(file.path(dir,file))
 km10
@@ -63,7 +70,8 @@ km10
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
+
 file <-"soc_0-5cm_mean_5000.tif"
 r <- raster(file.path(dir,file))
 
@@ -80,12 +88,12 @@ r
 summary(r)
 
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/10km/raster"
+dir <- file.path(gis_dir,"predictors/10km/raster")
 out_file <-"SOC.tif"
 raster::writeRaster(
   r,
   filename = file.path(dir,out_file),
-  forrmat ="GTiff" #, overwrite=TRUE
+  format ="GTiff" #, overwrite=TRUE
 )
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -93,7 +101,7 @@ raster::writeRaster(
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
 file <-"sand_0-5cm_mean_5000.tif"
 r <- raster(file.path(dir,file))
 
@@ -109,12 +117,12 @@ r <- round(raster::projectRaster(r,km10))
 r
 summary(r)
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"SAND.tif"
 raster::writeRaster(
   r,
   filename = file.path(dir,out_file),
-  forrmat ="GTiff" #, overwrite=TRUE
+  format ="GTiff" #, overwrite=TRUE
 )
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -123,7 +131,7 @@ raster::writeRaster(
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
 file <-"clay_0-5cm_mean_5000.tif"
 r <- raster(file.path(dir,file))
 
@@ -139,12 +147,12 @@ r <- round(raster::projectRaster(r,km10))
 r
 summary(r)
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"CLAY.tif"
 raster::writeRaster(
   r,
   filename = file.path(dir,out_file),
-  forrmat ="GTiff" #, overwrite=TRUE
+  format ="GTiff" #, overwrite=TRUE
 )
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -153,7 +161,7 @@ raster::writeRaster(
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
 file <-"bdod_0-5cm_mean_5000.tif"
 r <- raster(file.path(dir,file))
 
@@ -169,24 +177,24 @@ r <- round(raster::projectRaster(r,km10))
 r
 summary(r)
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"DENSITY.tif"
 
 raster::writeRaster(
   r,
   filename = file.path(dir,out_file),
-  forrmat ="GTiff" #, overwrite=TRUE
+  format ="GTiff" #, overwrite=TRUE
 )
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#### 6. SOIL.USDA USDA soil                                               ####
+#### 6. SOIL.USDA USDA soil great groups                                              ####
 # SoilGrids 1km data (Hengl2017) merged with metadata from He2020 & Hengl2019
 # forUSDA soil taxonomy (2014)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # Get SoilGrids (Hengl2017) metadata first
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster/metadata"
+dir <- file.path(gis_dir,"predictors/source/raster/metadata")
 file <-"TAXOUSDA_250m_ll.tif.csv"
 metadata <-read.csv(file.path(dir,file), na.strings = "")
 
@@ -240,7 +248,7 @@ rat <- rat %>%
 rat[1:10,]
 
 # Now get the raster
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
 file <-"TAXOUSDA_10km_ll.tif"
 r <- ratify(raster(file.path(dir,file)))
 r
@@ -261,7 +269,7 @@ summary(SOIL.USDA)
 
 summary(SOIL.USDA)
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"SOIL.USDA.tif"
 
 writeRaster(SOIL.USDA,
@@ -271,10 +279,10 @@ writeRaster(SOIL.USDA,
 plot(SOIL.USDA)
 SOIL.USDA <-NULL
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#### 7. SOIL.WRB  Soil type (WRB)                                         ####
+#### 7. SOIL.WRB  Soil group (WRB)                                         ####
 # SoilGrids 1km data (Hengl2017)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster/metadata"
+dir <- file.path(gis_dir,"predictors/source/raster/metadata")
 file <-"TAXNWRB_250m_ll.tif.csv"
 
 metadata <-read.csv(file.path(dir,file))
@@ -283,7 +291,7 @@ metadata$WRB_group <- factor(metadata$WRB_group)
 rat <- data.frame(ID=metadata$Number, SOIL.WRB = metadata$WRB_group)
 rat
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/source/raster"
+dir <- file.path(gis_dir,"predictors/source/raster")
 file <-"TAXNWRB_10km_ll.tif"
 r <- ratify(raster(file.path(dir,file)))
 r
@@ -299,7 +307,7 @@ summary(r)
 SOIL.WRB <- raster::resample(r,km10, method = 'ngb')
 SOIL.WRB
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"SOIL.WRB.tif"
 
 writeRaster(SOIL.WRB,
@@ -324,7 +332,7 @@ SOIL.WRB <-NULL
 # although the paper says it is polygons I con only find an asc raster
 # asc file specs:
 # https://www.loc.gov/preservation/digital/formats/fdd/fdd000421.shtml
-# dir <- "/Users/fvrodriguez/Desktop/LiMW_GIS_2015.gdb"
+# dir <- file.path(gis_dir,"/Users/fvrodriguez/Desktop/LiMW_GIS_2015.gdb"
 # file <-"glim_wgs84_0point5deg.txt.asc"
 
 # I acquired a copy of a glim gbd database at
@@ -348,7 +356,7 @@ SOIL.WRB <-NULL
 #       using driver `OpenFileGDB' successful.
 # Layer: GLiM_export (Multi Polygon)
 
-dir <- "~/Desktop"
+dir <- file.path(gis_dir,"predictors/source/vector/")
 file <-"LiMW_GIS_2015.gdb"
 
 glim_poly <- sf::st_read(dsn = file.path(dir,file), layer = "GLiM_export")
@@ -409,7 +417,7 @@ sf_use_s2(FALSE)
 glim_poly <- st_make_valid(glim_poly)
 glim_poly <- st_crop(glim_poly, xmin = -180, ymin = -57, xmax = 180, ymax = 84)
 
-# dir <- "~/Desktop"
+# dir <- file.path(gis_dir,"~/Desktop"
 # file <-"LiMW_GIS_2015_EPSG_4326.RDA"
 # class(glim_poly)
 # sf_use_s2(TRUE)
@@ -417,7 +425,7 @@ glim_poly <- st_crop(glim_poly, xmin = -180, ymin = -57, xmax = 180, ymax = 84)
 # save(glim_poly, file=file.path(dir,file) )
 #last valid
 
-# dir <- "~/Desktop"
+# dir <- file.path(gis_dir,"~/Desktop"
 # file <-"LiMW_GIS_2015_EPSG_4326.RDA"
 # load(file=file.path(dir,file))
 
@@ -435,7 +443,7 @@ st_is_longlat(glim_poly)
 # resolution : 0.0912, 0.0898  (x, y)
 
 # Match to exactly resolution and extent of km10
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster/"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster/")
 file <-"PH.tif"
 
 km10  <- read_stars(file.path(dir,file))
@@ -446,6 +454,7 @@ class(glim_poly$z)
 rs <- st_rasterize(
   glim_poly %>% dplyr::select(z, Shape),
   template = km10,
+  # https://gdal.org/doxygen/gdal__alg_8h.html
   options = c("MERGE_ALG=REPLACE", "ALL_TOUCHED=FALSE")
 )
 
@@ -460,7 +469,7 @@ summary(rs)
 st_crs(rs)
 plot(rs)
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"BEDROCK.tif"
 
 write_stars(rs,file.path(dir,out_file))
@@ -477,54 +486,332 @@ write_stars(rs,file.path(dir,out_file))
 
 
 
-
-#### MAT Temperature ####
-#### MAP Precipitation ####
-
-#### BIOMES Biomes ####
-biome <-read.csv(
-  text="ID,BIOME
-1,Tundra
-3,Boreal
-5,Mediterranean
-7,Temperate")
-
-
-
-
-#### DEPTH Soil depth ####
-
-#### NPP Net Primary Production ####
-# I'll start with the 2018 NPP from modis
-# But I want the "long term" average over 2001-2019 from this database:
-# http://files.ntsg.umt.edu/data/NTSG_Products/MOD17/MODIS_250/modis-250-npp/
-# http://files.ntsg.umt.edu/data/NTSG_Products/MOD17/MODIS_250/modis-250-npp/modis-250-npp-2018.tif
-
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/rfin/10km/raster"
-file <-"modis-250-npp-2018.tif"
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 10. MATEMP Mean Annual Temperature 1970-2015####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <-"wc2.1_5m_bio_1.tif"
 r <- raster(file.path(dir,file))
 
 # first check for +proj=longlat +datum=WGS84
 summary(r)
 r
 summary(r)
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster/")
+file <-"PH.tif"
+km10 <- raster(file.path(dir,file))
+
 km10
 summary(km10)
 
 # Now project to km10
+r <- round(raster::projectRaster(r,km10))
+r
+summary(r)
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"MATEMP.tif"
+raster::writeRaster(
+  r,
+  filename = file.path(dir,out_file),
+  format ="GTiff" #, overwrite=TRUE
+)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 11. MAPRECIP Mean Annual  Precipitation 2000 2015?                   ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster/")
+file <-"PH.tif"
+km10 <- raster(file.path(dir,file))
+
+km10
+summary(km10)
+
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <-"wc2.1_5m_bio_12.tif"
+r <- raster(file.path(dir,file))
+
+# first check for +proj=longlat +datum=WGS84
+summary(r)
+r
+summary(r)
 
 
-dir <- "/Users/fvrodriguez/Projects/NCSU/06_GEA/GIS/predictors/10km/raster"
+
+# Now project to km10
+r <- round(raster::projectRaster(r,km10))
+r
+summary(r)
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"MAPRECIP.tif"
+raster::writeRaster(
+  r,
+  filename = file.path(dir,out_file),
+  format ="GTiff" #, overwrite=TRUE
+)
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 12. BIOMES Terrestrial Ecoregions of the world                       ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+library(sf)
+dir <- file.path(gis_dir,"predictors/source/vector/Ecoregions2017")
+file <- "Ecoregions2017.shp"
+ecoregion <- st_read(file.path(dir,file))
+# Bounding box:  xmin: -180 ymin: -89.89197 xmax: 180 ymax: 83.62313
+# Geodetic CRS:  WGS 84
+st_crs(ecoregion)$proj4string
+summary(ecoregion)
+
+
+# remove the "N/A" value row. coded as 11 in the map
+# it seems to correspond to Arctic/Antartic ice regions
+# I could change the  value to 0
+# replace "N/A" value from 11 to 0
+ecoregion$BIOME_NUM[ecoregion$BIOME_NAME == "N/A"] <- 0
+
+# get the correspondence between raster int values
+# and biome names
+
+biome <-  as.data.frame(table(ecoregion$BIOME_NUM,ecoregion$BIOME_NAME))
+biome <- biome[biome$Freq>0,-3]
+colnames(biome) <- c("ID","NAME")
+
+# do the same for colors
+
+color  <-  as.data.frame(table(ecoregion$COLOR_BIO,ecoregion$BIOME_NAME))
+color <- color[color$Freq>0,-3]
+colnames(color) <- c("COLOR","NAME")
+
+# Now I got the colors right!
+
+rat <- biome %>%
+  dplyr::inner_join(color, by="NAME") %>%
+  arrange(ID)
+
+rat$COLOR
+
+
+# this is the pallete goes with values in the vector file
+# '#FFEAAF' '#38A700' '#CCCD65' '#88CE66' '#00734C'
+# '#458970' '#7AB6F5' '#FEAA01' '#FEFF73' '#BEE7FF'
+# '#D6C39D' '#9ED7C2' '#FE0000' '#CC6767' '#FE01C4'
+
+# dir <- file.path(gis_dir,"predictors/source/raster/metadata"
+# file <- "biomes.csv"
+
+# write.csv( rat, file =file.path(dir,file), row.names = FALSE)
+
+rat <- read.csv( file = file =file.path(dir,file))
+
+ecoregion$BIOME_NAME<- factor(ecoregion$BIOME_NAME)
+
+ecoregion$BIOME_NAME <- factor(ecoregion$BIOME_NAME, levels=rat$NAME)
+table(ecoregion$BIOME_NAME)
+
+# Fine I'll crop it
+sf_use_s2(FALSE)
+ecoregion <- st_make_valid(ecoregion)
+ecoregion <- st_crop(ecoregion, xmin = -180, ymin = -57, xmax = 180, ymax = 84)
+
+head(ecoregion)
+# Finally it is cropped!
+
+# rasterize as star object
+# Target resolution and extent
+# resolution : 0.0912, 0.0898  (x, y)
+
+# Match to exactly resolution and extent of km10
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster/")
+file <-"PH.tif"
+
+km10  <- read_stars(file.path(dir,file))
+class(km10)
+
+st_is_longlat(ecoregion)
+
+rs <- st_rasterize(
+  ecoregion %>% dplyr::select(BIOME_NAME, geometry),
+  template = km10,
+  options = c("MERGE_ALG=REPLACE", "ALL_TOUCHED=FALSE")
+)
+
+class(rs)
+str(rs)
+rat$NAME
+levels(rs) <- factor(rat$NAME,levels=rat$NAME)
+
+class(rs)
+rat <- droplevels(rat)
+names(rs) <-"BIOME"
+dim(rs)
+summary(rs)
+st_crs(rs)
+
+
+
+
+# this pallete goes with this order
+# > rat$NAME
+# [1] "Tropical & Subtropical Moist Broadleaf Forests"
+# [2] "Tropical & Subtropical Dry Broadleaf Forests"
+# [3] "Tropical & Subtropical Coniferous Forests"
+# [4] "Temperate Broadleaf & Mixed Forests"
+# [5] "Temperate Conifer Forests"
+# [6] "Boreal Forests/Taiga"
+# [7] "Tropical & Subtropical Grasslands, Savannas & Shrublands"
+# [8] "Temperate Grasslands, Savannas & Shrublands"
+# [9] "Flooded Grasslands & Savannas"
+# [10] "Montane Grasslands & Shrublands"
+# [11] "Tundra"
+# [12] "Mediterranean Forests, Woodlands & Scrub"
+# [13] "Deserts & Xeric Shrublands"
+# [14] "Mangroves"
+
+# Plot the raster
+# quartz(height =9, width=16)
+# plot(rs, col = rat$COLOR,key.pos =4,key.width =lcm(8))
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"BIOME.tif"
+
+write_stars(rs,file.path(dir,out_file),
+            driver ="GTiff",type = "Byte")
+
+
+# yet to use this from He2020:
+# but they match the info on the data source for biomes
+# biome <-read.csv(
+#   text="ID,BIOME
+# 1,Tundra
+# 3,Boreal
+# 5,Mediterranean
+# 7,Temperate"
+# 9,Desert
+# 12, Tropics)
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 13. DEPTH Soil depth                                                 ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+library(ncdf4)
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <-"BDTICM_M_10km_ll.tif"
+r <-  raster(file.path(dir,file))
+
+# Now project to km10
+r <- round(raster::projectRaster(r,km10))
+r
+summary(r)
+names(r)[1] <- "DEPTH"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"DEPTH.tif"
+raster::writeRaster(
+  r,
+  filename = file.path(dir,out_file),
+  format ="GTiff" #, overwrite=TRUE
+)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 14. MANPP Mean Anual Net Primary Production 2000-2015                ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# I have the 2000-2015 average
+# http://files.ntsg.umt.edu/data/NTSG_Products/MOD17/GeoTIFF/MOD17A3/GeoTIFF_30arcsec/MOD17A3_Science_NPP_mean_00_15.tif
+# But I want the "long term" average over 2001-2019.
+# Geotiff available for USA, not global.
+# http://files.ntsg.umt.edu/data/NTSG_Products/MOD17/GeoTIFF/MOD17A3/GeoTIFF_30arcsec/
+
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <-"MOD17A3_Science_NPP_mean_00_15.tif"
+r <- raster(file.path(dir,file))
+
+# first check for +proj=longlat +datum=WGS84
+summary(r)
+r
+summary(r)
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+file <-"PH.tif"
+km10 <- raster(file.path(dir,file))
+km10
+
+summary(km10)
+
+# Now project to km10
+
+# Now project to km10
+r <- round(raster::projectRaster(r,km10))
+# Error: vector memory exhausted (limit reached?)
+# do it on the server
+summary(r)
+names(r)[1] <- "NPP"
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
 out_file <-"NPP.tif"
 raster::writeRaster(
   r,
   filename = file.path(dir,out_file),
-  forrmat ="GTiff"
+  format ="GTiff"
 )
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#### SLOPE Slope ####
-
-
-#### ELEVATION Elevation ####
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 15. SLOPE Slope                                                      ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+file <-"PH.tif"
+km10 <- raster(file.path(dir,file))
+km10
+
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <-"slope_10KMmd_GMTEDmd.tif"
+r <-  raster(file.path(dir,file))
+r
+summary(r)
+
+# quartz()
+# hist(r,breaks =0:60)
+
+quartz()
+plot(r)
+# Now project to km10
+r <- round(raster::projectRaster(r,km10, method="ngb"))
+summary(r)
+# quartz()
+# hist(s,breaks =0:60)
+# quartz(height = 9, width = 16)
+# plot(s)
+
+names(r)[1] <- "SLOPE"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"SLOPE.tif"
+raster::writeRaster(
+  r,
+  filename = file.path(dir,out_file),
+  format ="GTiff" , overwrite=TRUE
+)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### 16. ELEVATION Elevation ####
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dir <- file.path(gis_dir,"predictors/source/raster")
+file <- "elevation_10KMmd_GMTEDmd.tif"
+r <-  raster(file.path(dir,file))
+
+# Now project to km10
+r <- round(raster::projectRaster(r,km10))
+r
+summary(r)
+names(r)[1] <- "ELEVATION"
+dir <- file.path(gis_dir,"predictors/rfin/10km/raster")
+out_file <-"ELEVATION.tif"
+raster::writeRaster(
+  r,
+  filename = file.path(dir,out_file),
+  format ="GTiff" #, overwrite=TRUE
+)
+
+
